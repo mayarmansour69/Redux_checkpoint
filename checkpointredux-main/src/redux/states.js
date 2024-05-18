0fec01b42a3a90bc56ba458a354fb393d56937bf
mayarmansour69 <mallorcamansour@gmail.com>
2024-05-19 00:41:54 +0100

    Created files Redux

---
+++ checkpointredux-main/src/redux/states.js
@@ -0,0 +1,18 @@
+export let todos = [
+    {
+        id:1,
+       name: "bcaaccabjcakbjbca",
+        done:false
+
+    },
+    {
+        id:2,
+        name: "cnajcabcjkbakjcbjca",
+        done:false
+    },
+    {
+        id:3,
+       name: "nckankclzancazkckc:cak",
+        done:false 
+    },
+]
